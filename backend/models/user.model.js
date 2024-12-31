9d8ee4051e32fa6ff5236f9d78d60e9884d49ccc
Abiral Jain <abiral2724@gmail.com>
2024-12-31 15:38:39 +0530

    backend api done

---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose') ;
+
+const userSchema = mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true ,
+    },
+    lastname:{
+        type:String,
+        required:true ,
+    },
+    email:{
+        type:String,
+        required:true ,
+    },
+    password:{
+        type:String,
+        required:true ,
+    },
+    confirmPassword:{
+        type:String,
+        required:true ,
+    },
+    gender:{
+        type:String,
+        enum:['Male' ,'Female' ,'Prefer not to say'] ,
+        required:true,
+    },
+    selectfield : {
+        type:String,
+        enum:['Patients' ,' Doctors'] ,
+        required:true,
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model('User' ,userSchema) ;
